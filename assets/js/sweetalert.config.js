0d34805a41596ce92b1728ab8395d53835560f92
Tanuj Singh <singh.tanuj021091@gmail.com>
2022-07-01 17:23:45 +0530

    second commit

---
+++ assets/js/sweetalert.config.js
@@ -0,0 +1,7 @@
+Swal.fire({
+    // icon: 'success',
+    title: 'Oops...',
+    text: 'Something went wrong!',
+    allowOutsideClick: false,
+    // footer: '<a href>Why do I have this issue?</a>'
+})
